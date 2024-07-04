96945932fbfe2980fc9a536d8497d9b6ca72028a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2024-07-04 16:48:21 +0200

    feat: try netlify_edge preset

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   middleware: "./src/middleware.ts",
   server: {
-    preset: "netlify",
+    preset: "netlify_edge",
     prerender: {
       routes: ["/about"],
     },
